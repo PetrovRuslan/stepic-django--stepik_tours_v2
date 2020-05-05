11a29ef1027424018af0ad43cc5dd422ec71ea6f
Petrov Ruslan <petrov.r.e@gmail.com>
2020-05-06 00:34:36 +0200

    добавлена обработка для вывода данных

--- tours/views.py
+++ tours/views.py
@@ -1,42 +1,64 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.views import View
 from data import *
-
-# context['loop_times'] = range(1, 8)
+import random
 
 class MainView(View):
     def get(self, request, *args, **kwargs):
+        random_items = []
+        index_tours = {}
+        random_items = random.sample(tours.keys(), 6)
+        for i in random_items:
+            index_tours[i] = tours[i]
         return render(
             request, 'tours/index.html', context={
-                'tours': tours,
                 'title': title,
                 'subtitle': subtitle,
                 'description': description,
                 'departures': departures,
+                'index_tours': index_tours,
             }
         )
 
 class DepartureView(View):
-    def get(self, request, *args, **kwargs):
+    def get(self, request, departure, *args, **kwargs):
+        departure_value_tours = 0
+        departures_dict = {}
+        if departure not in departures:
+            raise Http404
+        else:
+            for key, value in tours.items():
+                if value['departure'] == departure:
+                    departure_value_tours = departure_value_tours + 1
+                    departures_dict[key] = value
+            departure = departures[departure]
         return render(
             request, 'tours/departure.html', context={
-                'tours': tours,
                 'title': title,
                 'subtitle': subtitle,
                 'description': description,
                 'departures': departures,
+                'departure': departure,
+                'departure_value_tours': departure_value_tours,
+                'departures_dict': departures_dict,
             }
         )
 
 class TourView(View):
-    def get(self, request, *args, **kwargs):
-        return render(
-            request, 'tours/tour.html', context={
-                'tours': tours,
-                'title': title,
-                'subtitle': subtitle,
-                'description': description,
-                'departures': departures,
-            }
-        )
+    def get(self, request, id, *args, **kwargs):
+        if id not in tours:
+            raise Http404
+        else:
+            tour = tours[id]
+            departure = departures[tour['departure']]
+            return render(
+                request, 'tours/tour.html', context={
+                    'title': title,
+                    'subtitle': subtitle,
+                    'description': description,
+                    'departure': departure,
+                    'tour': tour,
+                    'departures': departures,
+                }
+            )
