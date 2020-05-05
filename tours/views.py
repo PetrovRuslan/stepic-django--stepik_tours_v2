45ae9b3ef0b88fc7650d57f2a251c23dc82112c4
Petrov Ruslan <petrov.r.e@gmail.com>
2020-05-05 08:06:33 +0200

    передача данных в шаблоны

--- tours/views.py
+++ tours/views.py
@@ -1,21 +1,42 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.views import View
+from data import *
+
+# context['loop_times'] = range(1, 8)
 
 class MainView(View):
     def get(self, request, *args, **kwargs):
         return render(
-            request, 'tours/index.html',
+            request, 'tours/index.html', context={
+                'tours': tours,
+                'title': title,
+                'subtitle': subtitle,
+                'description': description,
+                'departures': departures,
+            }
         )
 
 class DepartureView(View):
     def get(self, request, *args, **kwargs):
         return render(
-            request, 'tours/departure.html',
+            request, 'tours/departure.html', context={
+                'tours': tours,
+                'title': title,
+                'subtitle': subtitle,
+                'description': description,
+                'departures': departures,
+            }
         )
 
 class TourView(View):
     def get(self, request, *args, **kwargs):
         return render(
-            request, 'tours/tour.html',
+            request, 'tours/tour.html', context={
+                'tours': tours,
+                'title': title,
+                'subtitle': subtitle,
+                'description': description,
+                'departures': departures,
+            }
         )
